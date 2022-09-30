3ef3a7cb6fbd79a1697f38a1a6d88746a433d46d
Povilas Kanapickas <povilas@radix.lt>
2022-09-30 04:25:55 +0300

    ios: Wrap long lines

--- ios/MobisaneUITests/MobisaneUITestsLaunchTests.mm
+++ ios/MobisaneUITests/MobisaneUITestsLaunchTests.mm
@@ -39,7 +39,8 @@
     // Insert steps here to perform after app launch but before taking a screenshot,
     // such as logging into a test account or navigating somewhere in the app
 
-    XCTAttachment *attachment = [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
+    XCTAttachment *attachment =
+        [XCTAttachment attachmentWithScreenshot:XCUIScreen.mainScreen.screenshot];
     attachment.name = @"Launch Screen";
     attachment.lifetime = XCTAttachmentLifetimeKeepAlways;
     [self addAttachment:attachment];
