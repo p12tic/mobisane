c72f84ee06ee8e3954457f3390ad20b685d4094f
Povilas Kanapickas <povilas@radix.lt>
2022-10-31 05:32:17 +0200

    3rdparty: Fetch repos in download script

--- 3rdparty/download.py
+++ 3rdparty/download.py
@@ -88,6 +88,7 @@ def prepare_dependency(root_path, overwrite, name, remote, commit):
     else:
         sh(['git', 'clone', remote, repo_path], cwd=root_path)
 
+    sh(['git', 'fetch', '-p'], cwd=repo_path)
     sh(['git', 'checkout', '-f', commit], cwd=repo_path)
     sh(['git', 'submodule', 'update', '--init', '--recursive'], cwd=repo_path)
 
